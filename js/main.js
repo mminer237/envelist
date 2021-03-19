cac2dbecaa915bd9232a2703d9f641db7e2f4834
Matthew Miner <mminer237@gmail.com>
2021-03-18 22:13:52 -0500

    Add envelope

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 "use strict"
 
+let envelope = document.getElementById("envelope");
+let returnAddressBox = document.getElementById("return-address");
+let addressesBox = document.getElementById("addresses");
+
 document.getElementById("start").onclick = () => {
 	
 };
+
+function updateFont(font) {
+	returnAddressBox.style.fontFamily = font;
+	addressesBox.style.fontFamily = font;
+}
+
+let sizes = {
+	"A10": [9.5, 4.125]
+};
+function updateEnvelopeSize(size = "A10") {
+	envelope.width = sizes[size][0] * 96;
+	envelope.height = sizes[size][1] * 96;
+}
