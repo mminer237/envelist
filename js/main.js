4d051fbee8176ded206bb7e7e7928a81698ba6c0
Matthew Miner <mminer237@gmail.com>
2022-01-19 14:09:58 -0600

    Add sizes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 const { jsPDF } = window.jspdf;
 
+const presetSelect = document.getElementById("preset");
+const widthInput = document.getElementById("width");
+const heightInput = document.getElementById("height");
 const envelope = document.getElementById("envelope");
 const returnAddressBox = document.getElementById("return-address");
 const addressesBox = document.getElementById("addresses");
@@ -33,13 +36,51 @@ function updateFont(font) {
 }
 
 const sizes = {
-	"A10": [9.5, 4.125]
-	// "A10": [10, 4.5]
+	"#6¼": [6, 3.5],
+	"#6¾": [6.5, 3.675],
+	"A2": [5.75, 4.375],
+	"#7": [6.75, 3.75],
+	"C6": [6.3779, 4.4882],
+	"Monarch": [7.5, 3.875],
+	"A6": [6.5, 4.75],
+	"#8⅝": [8.625, 3.625],
+	"#9": [8.875, 3.875],
+	"DL": [8.6666, 4.3333],
+	"#10": [9.5, 4.125],
+	"#11": [10.375, 4.5],
+	"#12": [11, 4.75],
+	"#14": [11.5, 5],
+	"C5": [9.0157, 6.3779]
 };
-function updateEnvelopeSize(size = "A10") {
-	envelope.style.width = sizes[size][0] * 101 + "px";
-	envelope.style.height = sizes[size][1] * 101 + "px";
-	return sizes[size];
+
+/* Populate the preset sizes */
+presetSelect.innerHTML = Object.keys(sizes).map((size, _) => `<option value="${size}">${size}</option>`).join("\n\t\t\t") + "\n\t\t<option value=\"custom\">Custom</option>";
+/* Reselect #10 as the default */
+presetSelect.value = "#10";
+
+/* Update values on preset selection */
+presetSelect.onchange = presetChanged;
+function presetChanged() {
+	if (presetSelect.value !== "custom") {
+		widthInput.value = sizes[presetSelect.value][0];
+		heightInput.value = sizes[presetSelect.value][1];
+		updateEnvelopeSize(presetSelect.value);
+	}
+}
+
+/* Update on input */
+widthInput.oninput = sizeInputChanged;
+heightInput.oninput = sizeInputChanged;
+function sizeInputChanged() {
+	presetSelect.value = "custom";
+	updateEnvelopeSize();
+}
+
+/* Resize the visual envelope */
+function updateEnvelopeSize() {
+	envelope.style.width = widthInput.value * 101 + "px";
+	envelope.style.height = heightInput.value * 101 + "px";
+	return [widthInput.value, heightInput.value];
 }
 updateEnvelopeSize();
 
