6c9ac6139f5ca83d1d0353f4e964c9f885db1d60
Matthew Miner <mminer237@gmail.com>
2023-04-14 10:51:45 -0500

    Add license to JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-"use strict"
+// @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache-2.0
+
+"use strict";
 
 const { jsPDF } = window.jspdf;
 
@@ -267,3 +269,5 @@ function printPDF(doc) {
 	doc.autoPrint({variant: 'non-conform'});
 	doc.output('dataurlnewwindow');
 }
+
+// @license-end
